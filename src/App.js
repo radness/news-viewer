70e927b94a1642dae5fbdfc61986cbebeae552e7
radness <yhs5128@gmail.com>
2020-03-07 00:36:31 +0900

    NewsItem 만들기

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import axios from 'axios';
 const App = () => {
   const [data, setData] = useState(null);
 
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  // const onClick = () => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  //     setData(response.data);
+  //   });
+  // };
+
+  // async(비동기) 적용
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=aa1040815b9d4e5dbd9e8bc1b058aa7a',
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
+
   return (
     <div>
       <div>
@@ -18,7 +31,7 @@ const App = () => {
         <textarea
           rows={7}
           value={JSON.stringify(data, null, 2)}
-          readOnly={true}
+          // readOnly={true}
         />
       )}
     </div>
