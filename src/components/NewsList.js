2436857463dfbedc1ba9889ad7299eee0d34298e
radness <yhs5128@gmail.com>
2020-03-07 00:54:47 +0900

    뉴스 리스트에 데이터 연동하기

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
 // 해당 컴포넌트에서 API를 요청한다.
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -24,14 +25,46 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(
+          'http://newsapi.org/v2/top-headlines?country=kr&apiKey=aa1040815b9d4e5dbd9e8bc1b058aa7a',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // 대기 중
+  if (loading) {
+    return <NewsListBlock>Loading...</NewsListBlock>;
+  }
+  // article 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
+  // articles 값이 유효할 때
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {/* 데이터를 불러와서 뉴스 데이터 배열을 map 함수를 사용하여 컴포넌트 배열로 변환할 때 신경써야하는 것. */}
+      {/* map 함수를 사용하기 전에 !articles를 조회하여 해당 값이 현재 null이 아닌지 검사해야한다!!! */}
+      {/* 이 과정이 없으면 데이터가 없을 때 null에는 map 함수가 없기 때문에 렌더링 과정에서 오류가 발생한다. */}
+      {articles.map(article => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
